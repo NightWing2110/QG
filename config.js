302181a993cbca4c2acd679fa98f6d4af087870c
NightWing2110 <luuquanghoangdy@gmail.com>
2022-02-26 23:18:55 +0700

    Upcode

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'My Internet Is 4G But My Heart Is For You',
+    introDesc: ` 🤪  🤪  🤪 `,
+    btnIntro: '^^HiHi^^',
+    title: 'Đi xem phim Batman không :v',
+    desc: '🦇 🦇 🦇',
+    btnYes: 'Oki😋 ',
+    btnNo: 'Không bao h:3',
+    question: 'Lý do?',
+    btnReply: 'Send',
+    reply: 'Phim hay, trai đẹp, gu của emmmmmmmmmmmm',
+    mess: 'Oki chốt đơn :v',
+    messDesc: '',
+    btnAccept: 'Okiiiii',
+    messLink: 'https://www.facebook.com/messages/t/100033961174820'
+}
